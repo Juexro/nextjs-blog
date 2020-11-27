c079d36cbb6b71cbb150e96d3c7fb82d87e63cea
juexro <juexro@163.com>
2020-11-27 14:48:56 +0800

    feat: update style

--- pages/article/apply-cert.js
+++ pages/article/apply-cert.js
@@ -1,25 +1,45 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Component, { yaml, toc } from '@/documents/apply-cert';
 import * as fetch from '@/utils/fetch';
 import Toc from '@/components/Toc';
+import ArticleCopyright from '@/components/ArticleCopyright';
 import style from '@/styles/Article.module.scss';
 import { useRef } from 'react';
 
-export default function Article({ current }) {
+export default function Article({ prev, current, next }) {
   const ref = useRef();
   const onAnchorClick = (data) => {
-    const anchor = ref.current.querySelector('h1,h2,h3,h4,h5,h6');
-    ref.current.scrollTop = anchor.offsetTop;
+    const anchor = ref.current.querySelectorAll('h1,h2,h3,h4,h5,h6')[data.i];
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth' });
+      // ref.current.scrollTo({ top: anchor.offsetTop - 32,  behavior: 'smooth' });
+    }
   };
 
   return (
-    <div className={style.container}>
-      <div className={style.article} ref={ref}>
-        {current.title}
-        <Component></Component>
-      </div>
-      <div className={style.toc}>
-        <Toc data={toc} onAnchorClick={onAnchorClick}></Toc>
+    <div className="container">
+      <div className={style.container} ref={ref}>
+        <div className={style.article}>
+          {current.title}
+          <Component></Component>
+          <ArticleCopyright></ArticleCopyright>
+          <div>
+            {
+              prev && (
+                <div>上一篇 <Link href={`/article/${prev.name}`}>{prev.title}</Link></div>
+              )
+            }
+            {
+              next && (
+                <div>下一篇 <Link href={`/article/${next.name}`}>{next.title}</Link></div>
+              )
+            }
+          </div>
+        </div>
+        <div className={style.toc}>
+          <Toc data={toc} onAnchorClick={onAnchorClick}></Toc>
+        </div>
       </div>
     </div>
   )
