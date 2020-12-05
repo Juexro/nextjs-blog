e1d55edcf1af3c12e8ae7dddd457bf94a178cc2b
juexro <juexro@163.com>
2020-12-05 19:09:39 +0800

    refactor: out of the configuration

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 module.exports = {
+  profile: {
+    name: 'Juexro',
+    github: 'https://github.com/Juexro',
+    mail: 'juexro@163.com',
+    avatar: '/avatar.jpg',
+    location: 'Su Zhou, China',
+    position: 'Web Developer'
+  },
   gitalk: {
     accessToken: '',
     clientID: '',
     clientSecret: '',
-    repo: 'nextjs-blog',
+    repo: '',
     owner: 'Juexro',
     admin: ['Juexro'],
   }
