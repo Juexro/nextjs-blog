2ef64f6a36ff3dadb1fb6cfa1f44e7aa9829c5fb
juexro <juexro@163.com>
2020-12-12 19:09:08 +0800

    fix: parameter name email

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   profile: {
     name: 'Juexro',
     github: 'https://github.com/Juexro',
-    mail: 'juexro@163.com',
+    email: 'juexro@163.com',
     avatar: '/avatar.jpg',
     location: 'Su Zhou, China',
     position: 'Web Developer'
